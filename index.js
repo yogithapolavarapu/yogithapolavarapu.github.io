84dc4d213a373f1bb56aa099bd421e1c0cb54f1a
yogitha <yogitha.vja@gmail.com>
2016-06-24 12:31:59 +0530

    added icon and simple changes in css and js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   $('#jump_to_profile_area').smoothScroll({speed:1000});
   $("button").click(
+	
 	function(){
 		var area = $(this).attr("href");
 		$(".content").hide();
