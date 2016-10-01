549d7e652999fdb9fce036b8e4f20f97e7778f97
yogitha <yogitha.vja@gmail.com>
2016-10-01 15:47:59 +0530

    feilds required

--- email.php
+++ email.php
@@ -4,16 +4,15 @@
     $message = $_POST['your-message'];
     $from = 'From: website';
     $to = 'yogitha.vja@gmail.com';
-    $subject = 'Hello';
+    $subject = 'Message from website';
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
     if ($_POST['submit']) {
       if ($name != '' && $email != '') {
-    if (mail ($to, $subject, $body, $from)) {
-        echo '<p>Your message has been sent!</p>';
-    } else {
+        if (mail ($to, $subject, $body, $from)) {
+          echo '<p>Your message has been sent!</p>';
+        } else {
         echo '<p>Something went wrong, go back and try again!</p>';
+      }
     }
-  }}
-
-
+  }
 ?>
